718e2dacffb2fbca9044ae6f4be0fd8c6518e1a7
Filip Bergander <filip.bergander1996@gmail.com>
2026-03-31 15:31:01 +0200

    Gått över till postgresql som databashanterare.

--- install.js
+++ install.js
@@ -1,24 +1,44 @@
 // Installationsfil för sqlite3-databasen
-const sqllite3 = require("sqlite3").verbose();
+const { Client } = require("pg");
+require("dotenv").config();
 
-// Skapa databas
-const db = new sqllite3.Database("./db/cv.db");
-
-// Skapa tabell
-db.serialize(() => {
-    db.run("DROP TABLE IF EXISTS course;");
-
-    db.run(`
-    CREATE TABLE course(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        code TEXT NOT NULL,
-        name TEXT NOT NULL,
-        progression TEXT NOT NULL,
-        syllabus TEXT NOT NULL,
-        posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-    );
-    `);
-    db.run(`INSERT INTO course (code, name, progression, syllabus) VALUES ("DT224G", "Introduktion till webbutveckling med HTML, CSS och JavaScript", "A", "https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT224G/"), ("DT200G", "Grafisk teknik för webb", "A", "https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT200G/"), ("DT068G", "Webbanvändbarhet", "B", "https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT068G/"), ("DT003G", "Databaser", "A", "https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT003G/"), ("DT211G", "Frontend-baserad webbutveckling", "B", "https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT211G/");
-`);
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-db.close();
+
+client.connect((error) => {
+    if (error) {
+        console.log("Fel vid anslutning till databasen:", error);
+        return;
+    } else {
+        console.log("Anslutningen till databasen lyckades!");
+        createTable();
+    }
+});
+
+async function createTable() {
+    try {
+        const response = await client.query(`
+        CREATE TABLE IF NOT EXISTS course (
+            id SERIAL PRIMARY KEY,
+            code VARCHAR(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            progression VARCHAR(255) NOT NULL,
+            syllabus TEXT NOT NULL,
+            posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        );
+        `);
+        console.log(response);
+    } catch (error) {
+        console.log("Fel när tabellen skapades:", error);
+    } finally {
+        await client.end();
+    }
+}
